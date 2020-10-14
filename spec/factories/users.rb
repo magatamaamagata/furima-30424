4db035055fb0967b03eacc2a7e78a210190ae07a
magatamaamagata <magatamaamagata@gmail.com>
2020-10-14 18:11:53 +0900

    Add test code and delete dest code

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,14 @@
 FactoryBot.define do
-  
   factory :user do
-    nickname              {Faker::Name.initials(number: 4)}
-    email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    nickname              { Faker::Name.initials(number: 4) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
     gimei = Gimei.name
-    family_name_kanji      {gimei.last.kanji}
-    first_name_kanji       {gimei.first.kanji}
-    family_name_katakana   {gimei.last.katakana}
-    first_name_katakana    {gimei.first.katakana}
-    birthday              {Faker::Date.in_date_period}
+    family_name_kanji      { gimei.last.kanji }
+    first_name_kanji       { gimei.first.kanji }
+    family_name_katakana   { gimei.last.katakana }
+    first_name_katakana    { gimei.first.katakana }
+    birthday { Faker::Date.in_date_period }
   end
 end
