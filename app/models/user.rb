224b7ebc2f1ab3feda061c8ca2948d09e6fe944a
magatamaamagata <magatamaamagata@gmail.com>
2020-10-14 17:51:17 +0900

    Add test code(user model)

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+    validates :family_name_kanji
+    validates :first_name_kanji
+    validates :family_name_katakana
+    validates :first_name_katakana
+  end
+
   validates :nickname, presence: true
-  validates :family_name_kanji, presence: true
-  validates :first_name_kanji, presence: true
-  validates :family_name_katakana, presence: true
-  validates :first_name_katakana, presence: true
   validates :birthday, presence: true
+  validates :password, format: { with: /[a-z\d]{6,}/i, message: "は6文字以上の英数字が使えます" }
 
 end
