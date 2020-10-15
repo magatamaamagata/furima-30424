63fb5c4290175dee587240d39347c110e8bcf855
magatamaamagata <magatamaamagata@gmail.com>
2020-10-15 14:41:32 +0900

    Fix test code5
    Add with_options

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
-    validates :family_name_kanji
-    validates :first_name_kanji
+  with_options presence: true do
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+      validates :family_name_kanji
+      validates :first_name_kanji
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/, message: 'に全角カナを使用してください' } do
+      validates :family_name_katakana
+      validates :first_name_katakana
+    end
+    validates :nickname
+    validates :birthday
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'に全角カナを使用してください' } do
-    validates :family_name_katakana
-    validates :first_name_katakana
-  end
-
-  validates :nickname, presence: true
-  validates :birthday, presence: true
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-zA-Z\d]{6,50}+\z/, message: 'は6文字以上の英数字が使えます' }
+
 end
