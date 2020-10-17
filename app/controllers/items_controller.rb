046f17afdcc5f761d190f0e84b64b4b7709b110b
magatamaamagata <magatamaamagata@gmail.com>
2020-10-17 15:17:58 +0900

    Update items/index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
   def index
+    @items = Item.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -14,6 +15,9 @@ class ItemsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def show
   end
 
   private
