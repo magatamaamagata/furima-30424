7dc41db2053dd3b19180e627d2b56d7c3e81e9b0
magatamaamagata <magatamaamagata@gmail.com>
2020-10-18 13:19:54 +0900

    Update orders/index

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,22 @@
 class OrdersController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @order = Order.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.valid?
+      @order.save
+      return redirect_to root_path
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def order_params
+    # params.require(:order).merge(user_id: current_user.id,token: params[:token], price: @item.price)あとで
   end
 end
