8bdf0715acb75a3c8eecaba07db2fd856d135a42
magatamaamagata <magatamaamagata@gmail.com>
2020-10-20 13:59:54 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,18 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    
+    @item = Item.find(params[:id])
+    binding.pry
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
   private
 
   def item_params
