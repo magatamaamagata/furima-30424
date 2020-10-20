280f11dfac11a9fdcdbec8a143bc1580bbc4cb9a
magatamaamagata <magatamaamagata@gmail.com>
2020-10-20 14:21:29 +0900

    Add edit function

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,12 +22,16 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    
     @item = Item.find(params[:id])
-    binding.pry
   end
 
   def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def delete
