26c3b83416f20da565c58ab4adf79585ba60e881
magatamaamagata <magatamaamagata@gmail.com>
2020-11-26 23:41:03 +0900

    Apply S3 bucket

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root to: "items#index"
   devise_for :users
   resources :items do
-    resources :orders
+    resources :orders do
+      resources :comments
+    end
   end
 end
